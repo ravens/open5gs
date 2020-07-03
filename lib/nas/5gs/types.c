c7ad68f17edc6f01e8ec785a3124a6aa6eddff4d
Sukchan Lee <acetcom@gmail.com>
2020-07-02 20:40:26 -0400

    update it

--- lib/nas/5gs/types.c
+++ lib/nas/5gs/types.c
@@ -91,38 +91,37 @@ void ogs_nas_5gs_tai_list_build(
     }
 }
 
-void ogs_nas_build_s_nssai(
-        ogs_nas_s_nssai_t *nas_s_nssai, ogs_s_nssai_t *s_nssai)
+int ogs_nas_build_s_nssai(uint8_t *buffer, ogs_s_nssai_t *s_nssai)
 {
     int pos;
     ogs_uint24_t v;
-    ogs_assert(nas_s_nssai);
-    ogs_assert(s_nssai);
 
-    memset(nas_s_nssai, 0, sizeof(*nas_s_nssai));
+    ogs_assert(buffer);
+    ogs_assert(s_nssai);
 
     pos = 1;
 
     if (s_nssai->sst)
-        nas_s_nssai->buffer[pos++] = s_nssai->sst;
+        buffer[pos++] = s_nssai->sst;
 
     if (s_nssai->sd.v != OGS_S_NSSAI_NO_SD_VALUE) {
         v = ogs_htobe24(s_nssai->sd);
-        memcpy(nas_s_nssai->buffer+pos, &v, 3);
+        memcpy(buffer+pos, &v, 3);
         pos += 3;
     }
 
     if (s_nssai->mapped_hplmn_sst)
-        nas_s_nssai->buffer[pos++] = s_nssai->mapped_hplmn_sst;
+        buffer[pos++] = s_nssai->mapped_hplmn_sst;
 
     if (s_nssai->mapped_hplmn_sd.v != OGS_S_NSSAI_NO_SD_VALUE) {
         v = ogs_htobe24(s_nssai->mapped_hplmn_sd);
-        memcpy(nas_s_nssai->buffer+pos, &v, 3);
+        memcpy(buffer+pos, &v, 3);
         pos += 3;
     }
 
-    nas_s_nssai->buffer[0] = pos-1;
-    nas_s_nssai->length = pos;
+    buffer[0] = pos-1;
+
+    return pos;
 }
 
 int ogs_nas_parse_s_nssai(ogs_s_nssai_t *s_nssai, uint8_t *buffer)
@@ -204,7 +203,9 @@ void ogs_nas_build_nssai(ogs_nas_nssai_t *nas_nssai,
 
     for (i = 0; i < num_of_s_nssai; i++) {
         if (nas_nssai->length < OGS_NAS_MAX_NSSAI_LEN) {
-            ogs_nas_build_s_nssai(&nas_s_nssai, s_nssai + i);
+            nas_s_nssai.length = ogs_nas_build_s_nssai(
+                    nas_s_nssai.buffer, s_nssai + i);
+            ogs_assert(nas_s_nssai.length);
             memcpy(nas_nssai->buffer, nas_s_nssai.buffer, nas_s_nssai.length);
             nas_nssai->length += nas_s_nssai.length;
         }
